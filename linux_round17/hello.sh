0ce3b1cb4bf37fd3e72db46acc27a4b314b8143c
yichieh <buki3918@gmail.com>
2026-05-29 19:42:52 +0800

    Add Linux round 17 permission practice

---
+++ linux_round17/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello from Linux shell script"
+echo "Current folder:"
+pwd
+echo "List files:"
+ls
---
+++ linux_round17/project_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "=== Linux Project Info ==="
+echo "Current user:"
+whoami
+
+echo "Current path:"
+pwd
+
+echo "Current date:"
+date
+
+echo "Files in this folder:"
+ls -l
