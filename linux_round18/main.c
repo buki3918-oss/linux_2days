263516016e5c8fe676f3fa030e42eb2a96daa377
yichieh <buki3918@gmail.com>
2026-06-01 11:51:47 +0800

    Add Linux round 18 C compilation practice

---
+++ linux_round18/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from C on Linux!\n");
+    printf("This line will cause an error.\n");
+    return 0;
+}
---
+++ linux_round18/main_ok.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from C on Linux!\n");
+    printf("This is my first C program in WSL.\n");
+    return 0;
+}
